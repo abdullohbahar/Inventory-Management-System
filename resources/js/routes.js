c23b9d346d9aa31ddbe89474d5f1fe47b8a18bf4
AbdullohBahar <abdullohbahar@gmail.com>
2022-02-09 22:26:04 +0700

    implement vue-router

---
+++ resources/js/routes.js
@@ -0,0 +1,10 @@
+// Login Page
+let login = require("./components/auth/login.vue").default;
+
+// register Page
+let register = require("./components/auth/register.vue").default;
+
+export const routes = [
+    { path: "/", component: login, name: "/" },
+    { path: "/register", component: register, name: "register" },
+];
