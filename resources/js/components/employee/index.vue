a72269c85724e25a24f699628b5c2928282200fc
AbdullohBahar <abdullohbahar@gmail.com>
2022-02-15 20:51:20 +0700

    get data from database

--- resources/js/components/employee/index.vue
+++ resources/js/components/employee/index.vue
@@ -31,13 +31,97 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <router-link
-                                    to="/store-employee"
-                                    class="btn btn-primary"
-                                    >Add Employee</router-link
-                                >
+                                <h3 class="card-title">
+                                    <router-link
+                                        to="/store-employee"
+                                        class="btn btn-primary"
+                                        >Add Employee</router-link
+                                    >
+                                </h3>
                             </div>
-                            <div class="card-body"></div>
+
+                            <div class="card-body p-0">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Photo</th>
+                                            <th>Phone Number</th>
+                                            <th>Sallary</th>
+                                            <th>Joining Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr
+                                            v-for="employee in employees"
+                                            :key="employee.id"
+                                        >
+                                            <td>{{ employee.name }}</td>
+                                            <td>
+                                                <img
+                                                    :src="employee.photo"
+                                                    alt=""
+                                                    srcset=""
+                                                    id="em_photo"
+                                                />
+                                            </td>
+                                            <td>{{ employee.phone }}</td>
+                                            <td>{{ employee.sallary }}</td>
+                                            <td>{{ employee.joining_date }}</td>
+                                            <td>
+                                                <div class="btn-group">
+                                                    <button
+                                                        type="button"
+                                                        class="btn btn-info"
+                                                    >
+                                                        <i
+                                                            class="far fa-eye"
+                                                        ></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        class="btn btn-warning"
+                                                    >
+                                                        <i
+                                                            class="far fa-edit"
+                                                        ></i>
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        class="btn btn-danger"
+                                                    >
+                                                        <i
+                                                            class="far fa-trash-alt"
+                                                        ></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="card-footer clearfix">
+                                <ul
+                                    class="pagination pagination-sm m-0 float-right"
+                                >
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">«</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">1</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">2</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">3</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">»</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -56,8 +140,29 @@ export default {
             this.$router.push({ name: "/" });
         }
     },
-    methods: {},
+
+    data() {
+        return {
+            employees: [],
+        };
+    },
+    methods: {
+        allEmployee() {
+            axios
+                .get("/api/employee")
+                .then(({ data }) => (this.employees = data))
+                .catch();
+        },
+    },
+    created() {
+        this.allEmployee();
+    },
 };
 </script>
 
-<style lang=""></style>
+<style type="text/css">
+#em_photo {
+    height: 50%;
+    width: 50%;
+}
+</style>
